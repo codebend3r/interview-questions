270ce8710fe1b6c60266c0af1ccca91ea8b63b52
Mario Murray <mrmariomurray@gmail.com>
2021-05-10 17:52:02 -0400

    did my own examples

--- learning/basics/session-3.js
+++ learning/basics/session-3.js
@@ -46,12 +46,12 @@ const isBoolean = (a) => {
   return typeof a === 'boolean'
 }
 
-isBoolean(932)
-isBoolean(-10)
-isBoolean(0)
-isBoolean(true)
-isBoolean(false)
-isBoolean(9 === 9)
+// isBoolean(932)
+// isBoolean(-10)
+// isBoolean(0)
+// isBoolean(true)
+// isBoolean(false)
+// isBoolean(9 === 9)
 
 // Q2
 // Create a function that takes in a function as a param and will EXECUTE it IF it's a valid function
@@ -84,26 +84,78 @@ function makeStuffHappen() {
   console.log('boom')
 }
 
-typeof isFunctionAndCall(doMath)
-typeof isFunctionAndCall(null)
-typeof isFunctionAndCall('mario')
-typeof isFunctionAndCall(doStuff.bind(null, 'Chester'))
-typeof isFunctionAndCall(addTwoNumbers.bind(null, 44, 56))
-typeof isFunctionAndCall(() => {})
-typeof isFunctionAndCall(makeStuffHappen)
-typeof isFunctionAndCall(33)
+// typeof isFunctionAndCall(doMath)
+// typeof isFunctionAndCall(null)
+// typeof isFunctionAndCall('mario')
+// typeof isFunctionAndCall(doStuff.bind(null, 'Chester'))
+// typeof isFunctionAndCall(addTwoNumbers.bind(null, 44, 56))
+// typeof isFunctionAndCall(() => {})
+// typeof isFunctionAndCall(makeStuffHappen)
+// typeof isFunctionAndCall(33)
 
 const isDefined = (someVar) => {
   return typeof someVar !== 'undefined' && someVar !== null
 }
 
-isDefined(45)
-isDefined(0)
-isDefined(true)
-isDefined(false)
-isDefined(null)
-isDefined(undefined)
-isDefined([])
-isDefined({})
-isDefined('')
-isDefined('some story')
+// isDefined(45)
+// isDefined(0)
+// isDefined(true)
+// isDefined(false)
+// isDefined(null)
+// isDefined(undefined)
+// isDefined([])
+// isDefined({})
+// isDefined('')
+// isDefined('some story')
+
+//WHAT I LEARNED FROM THIS EXERCISE
+
+// 1. !== means is not
+// 2. to call a function you place () after the name of the function eg. isFunctionAndCall() to call the following function
+// const isFunctionAndCall = (someFunc) => {
+//   // typeof someFunc === 'function' && someFunc()
+
+//   if (typeof someFunc === 'function') {
+//     console.log('execute function')
+//     return someFunc()
+//   } else {
+//     console.log('not a function, do nothing')
+//   }
+// }
+// 3. return key word tells javascript that you'd like to know the value that was created when the function was executed.
+// 4. typeof always returns the type of the function as a string
+// 5. a parameter must be passed into a function to allow the function to assess itself eg.
+// adderFunc = (a) => {
+// allows someFunc to look for parameter a and evaluate it. Example below
+// }
+
+// MARIO'S EXAMPLES
+const adderFunc = (someNum) => {
+  typeof someNum === 'number'
+    ? console.log('this is a number')
+    : console.log('this is not a number')
+}
+
+// UNCOMMENT BELOW TO CALL THE ADDER FUNCTION
+// adderFunc('hello there')
+
+const adderFuncPlus = (someOtherNum) => {
+  if (typeof someOtherNum === 'number') {
+    console.log(someOtherNum + 20)
+  } else {
+    console.log('cannot do the math')
+  }
+}
+
+typeof adderFuncPlus('toodles')
+typeof adderFuncPlus(10 + 30)
+typeof adderFuncPlus(20)
+
+// const greatman = "I am a great man"
+// const greaterman = "I am greater man"
+
+// marioFunc(10)
+// marioFunc(10)
+// marioFunc(null)
+// marioFunc(greatman + greaterman)
+// marioFunc([])
