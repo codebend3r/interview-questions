1cced10371d0a1180da98c5c09416747580162aa
mrmariomurray <mrmariomurray@gmail.com>
2021-05-17 17:12:25 -0400

    I completed intermediate session 2

--- learning/intermediate/session-2.js
+++ learning/intermediate/session-2.js
@@ -2,13 +2,13 @@
 // The {zoo} object contains one property named {animals} which is another object
 // This is called a nested object (an object within an object)
 
-const defaultMakeNoise = () => "squeeeek";
+const defaultMakeNoise = () => 'squeeeek'
 
 const zoo = {
   animals: {
     lion: {
       makesNoise: () => {
-        return "ROAR";
+        return 'ROAR'
       },
     },
     giraffe: {
@@ -16,31 +16,36 @@ const zoo = {
     },
     monkey: {
       makesNoise: () => {
-        return "EEE EEE EEE";
+        return 'EEE EEE EEE'
       },
     },
     snake: {
       makesNoise: () => {
-        return "SSSSSSSS";
+        return 'SSSSSSSS'
       },
     },
   },
-};
+}
 
 // Q1
 // how do you call the {makeNoise} function in the {lion} object?
+console.log(zoo.animals.lion.makesNoise())
 
 // Q2
 // make a function that takes in an animal object (lion, giraffe, monkey, snake)
 // and checks the {makeNoise} function
 
-const doesAnimalMakeNoise = () => {};
-
-doesAnimalMakeNoise(zoo.animals.lion); // should return TRUE
-doesAnimalMakeNoise(zoo.animals.giraffe); // should return FALSE
-doesAnimalMakeNoise(zoo.animals.monkey); // should return TRUE
-doesAnimalMakeNoise(zoo.animals.snake); // should return TRUE
-
 //////////////////////////////////////////////////////
 //////////////////////////////////////////////////////
 // Please type out the code below.
+
+const doesAnimalMakeNoise = (a) => {
+  typeof a.makesNoise !== 'function'
+    ? console.log(a.makeNoise)
+    : console.log(a.makesNoise())
+}
+
+doesAnimalMakeNoise(zoo.animals.lion) // should return TRUE
+doesAnimalMakeNoise(zoo.animals.giraffe) // should return FALSE
+doesAnimalMakeNoise(zoo.animals.monkey) // should return TRUE
+doesAnimalMakeNoise(zoo.animals.snake) // should return TRUE
