7d629d6f1037a6181c7d8f8b7ee3feb54cb09300
Chester Rivas <crivas@varicent.com>
2021-05-20 11:43:05 -0400

    helped answer session 3 🦒
    - went over Object.entries
    - introduced tuples
    - learned about the Array map function
    - learned about key/value pairs
    - learned about spreading object
    - converted Object to Array

--- learning/intermediate/session-3.js
+++ learning/intermediate/session-3.js
@@ -29,59 +29,93 @@ const animals = {
     makesNoise: () => {
       return 'ROAR'
     },
+    weight: 500,
   },
   giraffe: {
     makeNoise: null,
+    weight: 300,
   },
   monkey: {
     makesNoise: () => {
       return 'EEE EEE EEE'
     },
+    weight: 200,
   },
   snake: {
     makesNoise: () => {
       return 'SSSSSSSS'
     },
+    weight: 3,
   },
 }
 
-// Q1. convert the zoo object to an array, it should look like this
+// Q1. convert the {animals} object to an array, it should look like this
 // HINT: https://reactgo.com/convert-object-to-array-javascript/
 
-const convertObjectToArray = (obj) => {
-  var zooConversion = Object.entries(obj.animals)
-  // var animalConversion = Object.fromEntries(obj.animals)
-  var zooValues = Object.values(zooConversion)
-  // console.log(zooValues)
-  var looping = zooValues.forEach(([]) => {
-    console.log(Object.fromEntries(zooValues))
-    // console.log('not quite there yet')
-  })
-}
+// Object.entries returns an array of tuples (an array of EXACTLY two things, length of 2)
+// we can then loop through each item in the array
+const convertedAnimalsArray = Object.entries(animals).map((animal) => {
+  // OPTION 1
+  // const key = animal[0] // animal at position 0 (first in the array)
+  // const value = animal[1] // animal at position 1 (second in the array)
 
-console.log(convertObjectToArray(zoo))
+  // OPTION 2
+  // destruct both items in the array to two constants from the animal tuple: key, value
+  const [key, value] = animal
 
-const convertedAnimalsArray = [
-  {
-    name: 'lion',
-    makesNoise: () => {
-      return 'ROAR'
-    },
-  },
-  {
-    name: 'giraffe',
-    makesNoise: null,
-  },
-  {
-    name: 'snake',
-    makesNoise: () => {
-      return 'SSSSSSSS'
-    },
-  },
-  {
-    name: 'monkey',
-    makesNoise: () => {
-      return 'EEE EEE EEE'
-    },
-  },
-]
+  // BOTH ARE DESTRUCTING
+  // const {} = something
+  // const [] = something
+
+  // key - lion - 0
+  // key - giraffe - 1
+  // key - monkey - 2
+  // key - snake - 3
+
+  // object signature is like this:
+  // animalObject = {
+  //   makeNoise,
+  //   weight
+  // }
+
+  // OPTION 1
+  // const mapObject = {}
+  // mapObject.name = key
+  // Object.keys(animalObject).forEach((innerKey) => {
+  //   mapObject[innerKey] = animalObject[innerKey]
+  // })
+  // return mapObject
+
+  // OPTION 2
+  return {
+    name: key,
+    ...value, // spread value object to fill in the rest
+  }
+})
+
+console.log('convertedAnimalsArray:', convertedAnimalsArray)
+
+// const convertedAnimalsArray = [
+//   {
+//     name: 'lion',
+//     makesNoise: () => {
+//       return 'ROAR'
+//     },
+//   },
+//   {
+//     name: 'giraffe',
+//     makesNoise: null,
+//   },
+//   {
+//     name: 'snake',
+//     makesNoise: () => {
+//       return 'SSSSSSSS'
+//     },
+//   },
+//   {
+//     name: 'monkey',
+//     makesNoise: () => {
+//       return 'EEE EEE EEE'
+//     },
+//   },
+// ]
